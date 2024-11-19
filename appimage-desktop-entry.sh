145847b02f59b79a746776787e69a13ec69c38f8
Ilya Shalyapin <ishalyapin@gmail.com>
2024-11-19 17:49:11 +0500

    fix shellcheck warnings; handle spaces in filenames

--- appimage-desktop-entry.sh
+++ appimage-desktop-entry.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+set -o pipefail
 
 APPIMAGE_PATH=$1
 
@@ -8,7 +10,7 @@ if [ -z "$APPIMAGE_PATH" ]; then
 fi
 
 if [ ! -f "$APPIMAGE_PATH" ]; then
-    echo "File not found:" $APPIMAGE_PATH
+    echo "File not found: $APPIMAGE_PATH"
     exit 1
 fi
 
@@ -23,17 +25,17 @@ cd /tmp/
 cd /tmp/squashfs-root/
 
 echo "Choose icon: "
-FILENAMES=($(find -L . -maxdepth 1 -type f \( -iname '*.png' -o -iname '*.svg' \)))
+mapfile -t FILENAMES < <(find -L . -maxdepth 1 -type f \( -iname '*.png' -o -iname '*.svg' \))
 i=1
-for filename in ${FILENAMES[*]}
+for filename in "${FILENAMES[@]}"
 do
-    printf " %d) %s\n" $i  $filename
-    i=$(expr $i + 1)
+    printf " %d) %s\n" "$i" "$filename"
+    i=$((i + 1))
 done
 
-read SELECTED_INDEX
+read -r SELECTED_INDEX
 
-ICON_SRC=${FILENAMES[$(expr $SELECTED_INDEX - 1)]}
+ICON_SRC=${FILENAMES[$((SELECTED_INDEX - 1))]}
 ICON_EXT="${ICON_SRC##*.}"
 ICON_FOLDER="${HOME}/.local/share/icons"
 ICON_DST="${ICON_FOLDER}/$APP_NAME.$ICON_EXT"             
@@ -42,21 +44,19 @@ cp "$ICON_SRC" "$ICON_DST"
 
 DESKTOP_ENTRY_PATH="${HOME}/.local/share/applications/$APP_NAME.desktop"
 
-APPIMAGE_FULLPATH_ESC_SPACES="${APPIMAGE_FULLPATH// /\\ }"
-
 cat <<EOT > "$DESKTOP_ENTRY_PATH"
 [Desktop Entry]
 Name=$APP_NAME
-Exec=$APPIMAGE_FULLPATH_ESC_SPACES
+Exec="$APPIMAGE_FULLPATH"
 Icon=$ICON_DST
 Type=Application
 Terminal=false
 EOT
 
-echo "Created";
+echo "Created"
 
 if [ "$2" == "--remove" ]; then
-    rm $ICON_DST
-    rm $DESKTOP_ENTRY_PATH
+    rm "$ICON_DST"
+    rm "$DESKTOP_ENTRY_PATH"
     echo "Removed."
 fi
